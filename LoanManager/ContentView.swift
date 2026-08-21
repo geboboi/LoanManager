a0494dfaabcb36eb1e5ff50af2680e6c3e2ac265
geboboi <146686496+geboboi@users.noreply.github.com>
2026-08-21 18:34:13 +0700

    Initial Commit

---
+++ LoanManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoanManager
+//
+//  Created by Gabriela on 21/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoanManager/LoanManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanManagerApp.swift
+//  LoanManager
+//
+//  Created by Gabriela on 21/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LoanManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
